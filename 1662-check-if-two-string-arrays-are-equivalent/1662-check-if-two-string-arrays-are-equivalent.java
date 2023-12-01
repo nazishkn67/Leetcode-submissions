6905019e5b5804fca07e383e3a8b357ce34c5cc0
Nazish Kamran <40017618+nazishkn67@users.noreply.github.com>
2023-12-01 11:04:56 +0530

    Time: 1 ms (73.30%), Space: 40.6 MB (28.67%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -3,11 +3,11 @@ class Solution {
         String firstWord = "";
         String secondWord = "";
 
-        for(int i = 0; i < word1.length; i++)
-            firstWord += word1[i];
+        for(String i: word1)
+            firstWord += i;
         
-        for(int j = 0; j < word2.length; j++)
-            secondWord += word2[j];
+        for(String j: word2)
+            secondWord += j;
         
         return (firstWord.equals(secondWord)) ? true : false;
     }
